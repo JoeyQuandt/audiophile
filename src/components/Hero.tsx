691c4966867ccf8a36a8d173dbb2c7062cae6cc0
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-10-13 14:07:39 +0200

    feat(hero component)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,32 +5,22 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="bg-[#121212] pt-24 pb-28">
+    <section className="bg-[#121212] max-lg:bg-[url('/images/hero_mobile.jpg')] lg:bg-[url('/images/hero_desktop.jpg')] max-lg:bg-cover lg:bg-contain bg-no-repeat bg-center pt-24 pb-40 ">
       <div className="layout">
-        <Separator className="mb-16" />
-        <section className="flex items-center justify-between text-theme-white">
+        <Separator className="lg:mb-16 mb-32" />
+        <section className="flex max-lg:text-center max-lg:justify-center items-center text-theme-white">
           <div>
-            <p className="mb-6 opacity-50 text-sm uppercase tracking-[10px]">
-              new product
-            </p>
-            <h1 className="mb-6">XX99 Mark II HeadphoneS</h1>
-            <p className="opacity-75 mb-10 font-medium max-w-[349px]">
+            <p className="mb-6 subtitleGray">new product</p>
+            <h1 className="mb-6 max-w-[396px] max-lg:mx-auto">
+              XX99 Mark II HeadphoneS
+            </h1>
+            <p className="opacity-75 mb-10 font-medium max-w-[349px] max-lg:mx-auto">
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
             <Button asChild>
               <Link href="/">See Product</Link>
             </Button>
-          </div>
-          <div className="image-wrapper">
-            <Image
-              src="/images/hero_headphone.png"
-              alt="Headphone image hero"
-              sizes="(max-width:768px) 100vw, 700px"
-              fill
-              priority
-              className="object-cover  min-h-[522px]"
-            />
           </div>
         </section>
       </div>
